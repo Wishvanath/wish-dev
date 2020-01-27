0bc4c84278d78e3e1d935b18857ed1be6ebb4555
Wishvanath SMG <anandwishvanath@gmail.com>
2020-01-27 13:10:19 +0530

    Updates

---
+++ precache-manifest.f8a9678d6651b41c65e2b394c9f5d043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36121539d0720ee7e221882dcd3f122d",
+    "url": "/wish-dev/index.html"
+  },
+  {
+    "revision": "67cf0a1a2a1329c2109c",
+    "url": "/wish-dev/static/css/main.b971b312.chunk.css"
+  },
+  {
+    "revision": "cb578df9c80d91596e2d",
+    "url": "/wish-dev/static/js/2.53ed7e31.chunk.js"
+  },
+  {
+    "revision": "f5ca32966347038a998bbf09dda684c7",
+    "url": "/wish-dev/static/js/2.53ed7e31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67cf0a1a2a1329c2109c",
+    "url": "/wish-dev/static/js/main.395fc65e.chunk.js"
+  },
+  {
+    "revision": "dc1895d7e5d84c578077",
+    "url": "/wish-dev/static/js/runtime-main.6e8ac1b0.js"
+  }
+]);
